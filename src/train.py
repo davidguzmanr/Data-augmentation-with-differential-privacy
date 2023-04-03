9e715c815bd0e247ba769a32ebfe0bf139aab0d0
David Guzmán <david_guzmanr@ciencias.unam.mx>
2023-04-03 17:57:31 -0400

    Add augmentation and configurations in train.py

--- src/train.py
+++ src/train.py
@@ -24,6 +24,7 @@ PATH_DATASETS = 'data'
 CIFAR10_MEAN = (0.4914, 0.4822, 0.4465)
 CIFAR10_STD = (0.2023, 0.1994, 0.2010)
 
+
 class MyLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
         # https://pytorch-lightning.readthedocs.io/en/stable/cli/lightning_cli_expert.html#argument-linking
@@ -33,14 +34,16 @@ class MyLightningCLI(LightningCLI):
 class CIFAR10DP(LightningModule):
     def __init__(
         self,
-        epochs: int = 500,
-        batch_size: int = 2048,
-        lr: float = 0.001,
+        epochs: int = 50,
+        batch_size: int = 128,
+        lr: float = 0.01,
+        optimizer_name: str = 'SGD',
+        weight_decay: float = 5e-3,
         differential_privacy: bool = False,
         data_augmentation: bool = False,
-        epsilon: float = 50.0,
-        delta: float = 1e-5,
-        max_grad_norm: float = 1.2,
+        epsilon: float = 10.0,
+        delta: float = 2e-5,
+        max_grad_norm: float = 1.0,
     ):
         super().__init__()
         self.save_hyperparameters()
@@ -51,26 +54,39 @@ class CIFAR10DP(LightningModule):
         self.epochs = epochs
         self.batch_size = batch_size
         self.lr = lr
+        self.weight_decay = weight_decay
+        self.optimizer_name = optimizer_name
         self.epsilon = epsilon
         self.delta = delta
         self.max_grad_norm = max_grad_norm
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         # Hardcode some dataset specific attributes
         self.num_classes = 10
         self.dims = (3, 32, 32)
         channels, width, height = self.dims
-        self.transform = transforms.Compose(
-            [
-                transforms.ToTensor(),
-                # If necessary, they can be computed with modest privacy budgets.
-                transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD)
-                # TODO: add data augmentation when data_augmentation=True
-            ]
-        )
 
-        # For the noise in the DP Gaussian mechanism and splitting train/val
-        self.generator = torch.Generator().manual_seed(2**42)
+        # Transformations to use in data augmentation
+        augmentations = [
+            transforms.RandomCrop(size=32, padding=4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5),
+        ]
+        normalize = [
+            transforms.ToTensor(),
+            # If necessary, they can be computed with modest privacy budgets.
+            transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
+        ]
+
+        if self.data_augmentation:
+            self.transform_train = transforms.Compose(augmentations + normalize)
+        else:
+            self.transform_train = transforms.Compose(normalize)
+
+        self.transform_eval = transforms.Compose(normalize)
+
+        # For the noise in the DP Gaussian mechanism
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.generator = torch.Generator(device=device).manual_seed(2**42)
 
         # Setup data
         self.prepare_data()
@@ -81,11 +97,12 @@ class CIFAR10DP(LightningModule):
         self.model = ModuleValidator.fix(models.resnet18(num_classes=10))
 
         if self.differential_privacy:
-            private_model, private_optimizer, private_train_loader = self.make_private()
-
-            self.model = private_model
-            self.private_optimizer = private_optimizer
-            self.private_train_loader = private_train_loader
+            (
+                self.privacy_engine,
+                self.private_model,
+                self.private_optimizer,
+                self.private_train_loader,
+            ) = self.make_private()
 
         # Define the metrics to evaluate
         self.train_accuracy = Accuracy(num_classes=self.num_classes)
@@ -106,8 +123,14 @@ class CIFAR10DP(LightningModule):
         self.train_accuracy(preds, y)
 
         # Calling self.log will surface up scalars for you in TensorBoard
-        self.log("train_loss", loss, prog_bar=True, on_step=True, on_epoch=False)
-        self.log("train_acc", self.train_accuracy, prog_bar=True, on_step=True, on_epoch=False)
+        self.log("train_loss", loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.log(
+            "train_acc",
+            self.train_accuracy,
+            prog_bar=True,
+            on_step=True,
+            on_epoch=True,
+        )
 
         return loss
 
@@ -120,16 +143,19 @@ class CIFAR10DP(LightningModule):
         # log step metric
         self.val_accuracy(preds, y)
 
-        # Calling self.log will surface up scalars for you in TensorBoard
-        self.log("val_loss", loss, prog_bar=True, on_step=True, on_epoch=False)
-        self.log("val_acc", self.val_accuracy, prog_bar=True, on_step=True, on_epoch=False)
+        self.log("val_loss", loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.log("val_acc", self.val_accuracy, prog_bar=True, on_step=True, on_epoch=True)
+
+        if self.differential_privacy:
+            epsilon = self.privacy_engine(self.delta)
+            self.log('epsilon', epsilon, on_step=True, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = F.cross_entropy(logits, y)
         preds = torch.argmax(logits, dim=1)
-        
+
         self.test_accuracy(preds, y)
 
         # Calling self.log will surface up scalars for you in TensorBoard
@@ -137,12 +163,23 @@ class CIFAR10DP(LightningModule):
         self.log("test_acc", self.test_accuracy, prog_bar=True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.lr, momentum=0.9, weight_decay=5e-4)
+        if self.optimizer_name == 'SGD':
+            optimizer = torch.optim.SGD(
+                self.parameters(), lr=self.lr, momentum=0.9, weight_decay=self.weight_decay
+            )
+        elif self.optimizer_name == 'Adam':
+            optimizer = torch.optim.Adam(
+                self.parameters(), lr=self.lr, weight_decay=self.weight_decay
+            )
 
         if self.differential_privacy:
-            return self.private_optimizer
+            optimizer = self.private_optimizer
 
-        return optimizer
+        scheduler = torch.optim.lr_scheduler.MultiplicativeLR(
+            optimizer, lr_lambda=lambda epoch: 0.1
+        )
+
+        return [optimizer], [{"scheduler": scheduler, "interval": "epoch", 'frequency': 20}]
 
     ####################
     # DATA RELATED HOOKS
@@ -154,10 +191,15 @@ class CIFAR10DP(LightningModule):
         CIFAR10(PATH_DATASETS, train=False, download=True)
 
     def setup(self, stage=None):
-        # Assign train/val/tests datasets for use in dataloaders
-        cifar10_full = CIFAR10(PATH_DATASETS, train=True, transform=self.transform)
-        self.cifar10_train, self.cifar10_val = random_split(cifar10_full, [48000, 2000], generator=self.generator)
-        self.cifar10_test = CIFAR10(PATH_DATASETS, train=False, transform=self.transform)
+        self.cifar10_train = CIFAR10(PATH_DATASETS, train=True, transform=self.transform_train)
+
+        # We will use test as validation to get plots like in https://arxiv.org/pdf/1607.00133.pdf
+        self.cifar10_val = CIFAR10(PATH_DATASETS, train=False, transform=self.transform_eval)
+        self.cifar10_test = CIFAR10(PATH_DATASETS, train=False, transform=self.transform_eval)
+
+        # self.cifar10_val, self.cifar10_test = random_split(
+        #     cifar10_test, [5000, 5000], generator=torch.Generator().manual_seed(42)
+        # )
 
     def train_dataloader(self):
         if self.differential_privacy:
@@ -180,12 +222,22 @@ class CIFAR10DP(LightningModule):
         objects: model, optimizer, and the train dataloader.
         """
         privacy_engine = PrivacyEngine()
-        train_loader = DataLoader(
-            self.cifar10_train, batch_size=self.batch_size, num_workers=4
-        )
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        train_loader = DataLoader(self.cifar10_train, batch_size=self.batch_size, num_workers=4)
 
-        private_model, private_optimizer, private_train_loader = privacy_engine.make_private_with_epsilon(
+        if self.optimizer_name == 'SGD':
+            optimizer = torch.optim.SGD(
+                self.parameters(), lr=self.lr, momentum=0.9, weight_decay=self.weight_decay
+            )
+        elif self.optimizer_name == 'Adam':
+            optimizer = torch.optim.Adam(
+                self.parameters(), lr=self.lr, weight_decay=self.weight_decay
+            )
+
+        (
+            private_model,
+            private_optimizer,
+            private_train_loader,
+        ) = privacy_engine.make_private_with_epsilon(
             module=self.model,
             optimizer=optimizer,
             data_loader=train_loader,
@@ -196,16 +248,13 @@ class CIFAR10DP(LightningModule):
             noise_generator=self.generator,
         )
 
-        return private_model, private_optimizer, private_train_loader
+        return privacy_engine, private_model, private_optimizer, private_train_loader
 
 
 def cli_main():
     # The LightningCLI removes all the boilerplate associated with arguments parsing. This is purely optional.
     cli = MyLightningCLI(
-        CIFAR10DP,
-        seed_everything_default=42,
-        save_config_overwrite=True,
-        run=False
+        CIFAR10DP, seed_everything_default=42, save_config_overwrite=True, run=False
     )
     cli.trainer.fit(cli.model, datamodule=cli.datamodule)
     cli.trainer.test(ckpt_path="best", datamodule=cli.datamodule)
